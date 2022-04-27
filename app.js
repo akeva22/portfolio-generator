ec53ab5ada516eb8527c36833959aa0a9747988e
Akeva Melchor <akeva.melchor@expel.io>
2022-04-26 23:30:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// console.log("Hello Node!")
+
+// var message = 'Hello Node!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+    });
+};
+
+printProfileData(profileDataArgs);
